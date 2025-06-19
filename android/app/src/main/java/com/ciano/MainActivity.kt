4162a33f987ed9c3965241503027d33106241c04
diegostafa <dstafa.dev@gmail.com>
2025-06-19 21:15:01 +0200

    update app name

--- android/app/src/main/java/com/ciano/MainActivity.kt
+++ android/app/src/main/java/com/ciano/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ciano"
+  override fun getMainComponentName(): String = "Ciano"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
